f3d2d1830c7a312e8a43c77a8d1312bea419d8d4
Aivaseda <aivaseda@bk.ru>
2024-09-22 09:38:57 +0500

    completed 3 point

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,10 @@ repositories { mavenCentral() }
 application { mainClass.set("hexlet.code.App") }
 
 dependencies {
+    implementation("com.h2database:h2:2.2.224")
+    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+    implementation("org.apache.commons:commons-text:1.11.0")
     implementation("gg.jte:jte:3.1.9")
     implementation("org.slf4j:slf4j-simple:2.0.9")
     implementation("io.javalin:javalin:6.1.3")
